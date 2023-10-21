83e01f5d5f212d0619a95d7c6a3ce66f5fced172
ReBo <ohsi_0615@naver.com>
2023-10-21 23:06:05 +0900

    메인 타이틀 추가
    시작 버튼이랑 종료 버튼 작동 가능함.

---
+++ One_Shot/Assets/Scripts/GameStarter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStarter : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
