ff93b1af78fb6ac3b4fb9ee90dffb5ed31b96175
yuno0271 <yuno0271@naver.com>
2023-10-21 23:10:49 +0900

    몬스터샘플

---
+++ One_Shot/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    // HP
+    public int hp = 1;
+    // 이동 속도
+    public float speed = 5.0f;
+    // 반응 거리
+    public float reactionDistance = 4.0f;
+    // 애니메이션 이름
+    public string idleAnime = "EnemyIdle";  // 정지
+    // 현재 애니메이션
+    string nowAnimation = "";
+    string oldAnimation = "";
+
+    float axisH;    // 가로축 값 (-1.0 ~ 0.0 ~1.0)
+    float axisV;    // 세로축 값 (-1.0 ~ 0.0 ~1.0)
+    Rigidbody2D rbody; // Rigidbody 2D
+
+    bool isActive = false;    // 활성 여부
+
+    public int arranged = 0;  // 배치 식별에 사용
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
