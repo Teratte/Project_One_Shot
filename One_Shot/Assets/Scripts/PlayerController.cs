4da1ea87b838abf0ebd618180fce50e218c4eb29
JaeHun Jin <ospf0306@gmail.com>
2023-10-26 03:04:18 +0900

    Player 회전 구현
    회전 360 구현

--- One_Shot/Assets/Scripts/PlayerController.cs
+++ One_Shot/Assets/Scripts/PlayerController.cs
@@ -5,37 +5,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float axisH;                // 가로축 값(-1.0 ~ 0.0 ~ 1.0)
-    float axisV;                // 세로축 값(-1.0 ~ 0.0 ~ 1.0)
-    public float angleZ = -90.0f; // 회전 각
-
-    bool isMoving = false;        // 이동 중인지 여부
-    
+    float angle;
+    Vector2 target, mouse;
     Rigidbody2D rbody;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        // Rigidbody2D 가져오기
         rbody = GetComponent<Rigidbody2D>();
+        target = transform.position;
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        Vector2 playerPosition = new Vector2(axisH, axisV);
-    }
-
-    public void SetAxis(float h, float v)
-    {
-        axisH = h;
-        axisV = v;
-        if(axisH == 0 && axisV == 0)
-        {
-            isMoving = false;
-        }
-        else
-        {
-            isMoving = true;
-        }
+        mouse = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        angle = Mathf.Atan2(mouse.y - target.y, mouse.x - target.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
     }
 }
